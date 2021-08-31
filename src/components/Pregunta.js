855ddb98ba449b395d2dad0007140a38f535069c
armie1 <armieone19@gmail.com>
2021-08-31 01:49:49 -0500

    FirstClass

---
+++ src/components/Pregunta.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useState } from 'react';
+
+const Pregunta = () => {
+
+    // Definir el state
+    const [ cantidad, guardarCantidad ] = useState(0);
+
+    // Función que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad( parseInt( e.target.value, 10) )
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+
+
+        // Si se pasa la validacion
+    }
+
+    return ( 
+        <Fragment
+        onSubmit={agregarPresupuesto}
+        >
+            <h2>Coloca tu presupuesto</h2>
+
+            <form>
+                <input 
+                type="number"
+                className="u-full-width"
+                placeholder="Coloca tu presupuesto"
+                onChange={definirPresupuesto}
+                />
+                <input 
+                type="submit"
+                className="button-primary u-full-width"
+                value="Definir el Presupuesto"
+                />
+            </form>
+        </Fragment>
+     );
+}
+ 
+export default Pregunta;
